3cf7858ca08923745b55e2867afb8e70d13c8e8e
Feroz <mdferozahmed27156@gmail.com>
2025-06-02 19:14:18 +0530

    Update claudinary.js

--- Backend/config/claudinary.js
+++ Backend/config/claudinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 
 const connectCloudinary = async () => {
   cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET_KEY,
+   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_NAME,
+CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+CLOUDINARY_API_SECRET: process.env.CLOUDINARY_SECRET_KEY,
   });
 };
 
